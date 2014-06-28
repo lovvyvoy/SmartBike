d4dc3e2d9d3bed38279044e58deefa50770e210f
BenjaminCordero <benjamin.cordero@alumnos.inf.utfsm.cl>
2014-06-27 22:39:48 -0400

    Segunda Temporada

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,11 +1,15 @@
-def google_maps_api_key
-	"AIzaSyD5vuFoJirfgRNu5y5HmlNmnb7dIDKjkp4"
-end
+module TracksHelper
 
-def google_api_url
-	"http://maps.googleapis.com/maps/api/js"
-end
+	def google_maps_api_key
+		"AIzaSyD5vuFoJirfgRNu5y5HmlNmnb7dIDKjkp4"
+	end
 
-def google_api_access
-	"#{google_api_url}?key=#{google_maps_api_key}&amp;libraries=geometry&amp;sensor=false"
+	def google_api_url
+		"http://maps.googleapis.com/maps/api/js"
+	end
+
+	def google_api_access
+		"#{google_api_url}?key=#{google_maps_api_key}&amp;libraries=geometry&amp;sensor=false"
+	end
+
 end
