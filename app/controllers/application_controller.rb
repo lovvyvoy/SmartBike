82f55ec7cc4db2cec8db61541bd87f04ce5a48b7
BenjaminCordero <benjamin.cordero@alumnos.inf.utfsm.cl>
2014-07-26 23:38:51 -0400

    Registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,24 @@ class ApplicationController < ActionController::Base
   end
 
   before_filter :ensure_signup_complete, only: [:new, :create, :update, :destroy]
+
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_devise_permitted_parameters
+    registration_params = [:name, :lastname, :disp, :email, :password, :password_confirmation, :age, :height, :weight]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_udpate) {
+        |u| u.permit(registration_params << :current_password)
+      }
+
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) {
+        |u| u.permit(registration_params)
+      }
+
+    end
+  end
 end
