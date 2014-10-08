9ea8d0129b58c052ed5cd855ac9cc7b6b5a1f716
BenjaminCordero <benjamin.cordero@alumnos.inf.utfsm.cl>
2014-10-08 19:06:36 -0300

    api

--- app/models/geofence.rb
+++ app/models/geofence.rb
@@ -1,7 +1,6 @@
 class Geofence < ActiveRecord::Base
 	
 	reverse_geocoded_by :x_act, :y_act
-
-	after_validation :reverse_geocoded
+	after_validation :reverse_geocode, :if => :x_act_changed?
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915203354) do
+ActiveRecord::Schema.define(version: 20141008202823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 20140915203354) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "address"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
