2b2fb5a5a05192f189de6aa157ac30a7dd9461a1
BenjaminCordero <benjamin.cordero@alumnos.inf.utfsm.cl>
2014-10-09 17:26:18 -0300

    wea

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008202823) do
+ActiveRecord::Schema.define(version: 20141009201559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,5 +70,15 @@ ActiveRecord::Schema.define(version: 20141008202823) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "viajes", force: true do |t|
+    t.integer  "id_viaje"
+    t.integer  "user_id"
+    t.float    "distancia"
+    t.integer  "tiempo"
+    t.date     "fecha"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
