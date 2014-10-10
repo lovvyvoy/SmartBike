cc94feccbc2779a17b7a4fd11058bfdd86acae7b
BenjaminCordero <benjamin.cordero@alumnos.inf.utfsm.cl>
2014-10-10 14:41:03 -0300

    Int1

--- app/models/geofence.rb
+++ app/models/geofence.rb
@@ -2,21 +2,23 @@ class Geofence < ActiveRecord::Base
 
 	reverse_geocoded_by :y_act, :x_act
 	after_validation :reverse_geocode
-	after_update :alarma
+	after_update :alarma, if: :alarm?
 
 	# private
 
-	# def alarma
-	# 	$pubnub = Pubnub.new(
-	# 	    :publish_key => 'pub-c-ecd86a1b-4ad4-4b48-9d34-8b07cf3d8c3f', 
-	# 	    :subscribe_key => 'sub-c-380453a0-4f40-11e4-b29a-02ee2ddab7fe'
-	# 	)
+	 def alarma
+	 	if this.distance_to([this.y_al, this.y]) > 0.5
 
-	# 	$pubnub.publish(
-	#         :channel => 'SmartBike',
-	#         :message => 'alerta',
-	#         :callback => $callback
-	# 	)
-	# end
-	
+		 	$pubnub = Pubnub.new(
+		 	    :publish_key => 'pub-c-ecd86a1b-4ad4-4b48-9d34-8b07cf3d8c3f', 
+		 	    :subscribe_key => 'sub-c-380453a0-4f40-11e4-b29a-02ee2ddab7fe'
+		 	)
+
+		 	$pubnub.publish(
+		         :channel => 'SmartBike',
+		         :message => 'alerta',
+		         :callback => $callback
+		 	)
+		end
+	end
 end
