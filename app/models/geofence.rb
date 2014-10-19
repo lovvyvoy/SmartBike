b74d5f652e95bf04a8f4e75e0a772d4cd96c5cf0
BenjaminCordero <benjamin.cordero@alumnos.inf.utfsm.cl>
2014-10-19 16:17:04 -0300

    Try

--- app/models/geofence.rb
+++ app/models/geofence.rb
@@ -1,3 +1,8 @@
+pubnub = Pubnub.new(
+    :publish_key => 'pub-c-ecd86a1b-4ad4-4b48-9d34-8b07cf3d8c3f', 
+    :subscribe_key => 'sub-c-380453a0-4f40-11e4-b29a-02ee2ddab7fe'
+)
+
 class Geofence < ActiveRecord::Base
 
 	reverse_geocoded_by :y_act, :x_act
@@ -7,18 +12,10 @@ class Geofence < ActiveRecord::Base
 	# private
 
 	 def alarma
-	 	if this.distance_to([this.y_al, this.y]) > 0.5
-
-		 	$pubnub = Pubnub.new(
-		 	    :publish_key => 'pub-c-ecd86a1b-4ad4-4b48-9d34-8b07cf3d8c3f', 
-		 	    :subscribe_key => 'sub-c-380453a0-4f40-11e4-b29a-02ee2ddab7fe'
-		 	)
-
-		 	$pubnub.publish(
+	 	#if this.distance_to([this.y_al, this.y]) > 0.5
+		 	pubnub.publish(
 		         :channel => 'SmartBike',
-		         :message => 'alerta',
-		         :callback => $callback
+		         :message => 'alerta'
 		 	)
-		end
 	end
 end
