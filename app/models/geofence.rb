06986805ec478114f428a0c0acb7334b1403b191
BenjaminCordero <benjamin.cordero@alumnos.inf.utfsm.cl>
2014-11-02 19:03:58 -0300

    Domingo

--- app/models/geofence.rb
+++ app/models/geofence.rb
@@ -3,12 +3,4 @@ class Geofence < ActiveRecord::Base
 	reverse_geocoded_by :y_act, :x_act
 	after_validation :reverse_geocode, :alarma
 
-	# private	 
-	def alarma
-		$pubnub.publish(
-		  :channel => 'SmartBike',
-		  :message => 'alerta',
-		  :callback => $callback
-		)
-	end
 end
