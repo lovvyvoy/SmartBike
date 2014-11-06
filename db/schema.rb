dfd3ee904bf382c4ca54a94f98fc84d593964f44
BenjaminCordero <benjamin.cordero@alumnos.inf.utfsm.cl>
2014-11-06 15:24:47 -0300

    Refresh

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-  # encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20141024182224) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.date     "date_logrado"
   end
 
   create_table "maps", force: true do |t|
