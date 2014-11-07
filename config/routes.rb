638b52df11ea4759aa78d122f84355855fa3a3b3
BenjaminCordero <benjamin.cordero@alumnos.inf.utfsm.cl>
2014-11-07 01:28:36 -0300

    Refresh

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
+  resources :users, :only => [:show]
+  
+  get 'users/:id' => 'users#show'
+
   get 'welcome/index'
 
   resources :geofences
